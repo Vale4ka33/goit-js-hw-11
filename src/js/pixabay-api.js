a07ebd855f7a4124764449cc2987dfeb952d39a8
Vale4ka33 <150661954+Vale4ka33@users.noreply.github.com>
2024-04-07 23:53:02 +0300

    hw-11 start

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,2 @@
+const API_KEY = "43261238 - 3a65c2c3cbe2ed873fa1b88e3";
+const URL = "https://pixabay.com/api/";
--- vite.config.js
+++ vite.config.js
@@ -11,20 +11,12 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
-
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
-        },
-      },
       outDir: '../dist',
+
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html'])
+    ],
   };
 });
