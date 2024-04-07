1bf661df615bc109d54a72010492571b8b79a260
Vale4ka33 <150661954+Vale4ka33@users.noreply.github.com>
2024-04-08 01:26:55 +0300

    hw 11- done without library yet

--- src/main.js
+++ src/main.js
@@ -2,74 +2,34 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import { searchImage } from './js/pixabay-api';
+import { createMarkup, initLightbox } from './js/render-funcions';
 
 const form = document.querySelector('.form');
 const imgList = document.querySelector('.img-list');
+const loader = document.querySelector('.loader');
 
-const API_KEY = "43261238-3a65c2c3cbe2ed873fa1b88e3";
-const URL = "https://pixabay.com/api/";
 
 form.addEventListener('submit', submitForm);
 
 function submitForm(event) {
     event.preventDefault();
 
-    const searchInput = event.currentTarget.elements.search.value;
+    const searchInput = event.currentTarget.elements.search.value.trim();
+    if (searchInput === '') {
+        return;
+    }
+    loader.style.display = 'block';
+     imgList.innerHTML = '';
+
     searchImage(searchInput)
         .then(data => {
             console.log(data);
             imgList.innerHTML = createMarkup(data.hits)
             initLightbox();
+
+            form.reset();
         })
         .catch(error => alert(error));
-
-
-}
-
-function searchImage(searchInput = '') {
-    const params = new URLSearchParams({
-        key: API_KEY,
-        q: searchInput,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true
-    })
-
-    return fetch(`${URL}?${params}`)
-        .then(response => {
-            if (!response.ok) {
-            throw new Error("Sorry, there are no images matching your search query. Please try again!")
-            }
-            return response.json();
-        })
-        .catch(error => {
-            throw new Error("Sorry, an error occurred while fetching data. Please try again later!");
-        });
-}
-
-function createMarkup(arr) {
-    return arr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
-        <li class="img-item">
-            <a class="img-link" href="${largeImageURL}">
-                <img class="image" src="${webformatURL}" alt="${tags}">
-                <ul class="info-card">
-                    <li><span class="info-label">Likes</span><br>${likes}</li>
-                    <li><span class="info-label">Views</span><br>${views}</li>
-                    <li><span class="info-label">Comments</span><br>${comments}</li>
-                    <li><span class="info-label">Downloads</span><br>${downloads}</li>
-                </ul>
-            </a>
-        </li>`
-    ).join("");
-}
-
-function initLightbox() {
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-  animationSpeed: 250,
-  animationSlide: true
-})
-    lightbox.refresh();
+     loader.style.display = 'none';
 }
